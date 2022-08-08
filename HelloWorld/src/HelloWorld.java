6b9190064f81b5db81ea45328a424527abf8508b
unknown <xogk1128@naver.com>
2022-08-08 16:15:19 +0900

    데이터와 연산

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println(6);	// Number
+		System.out.println("6");// String
+		
+		System.out.println(6+6); // 12
+		System.out.println("6"+"6"); // 66
+		
+		System.out.println("1111".length()); // 4
+	}
+}
