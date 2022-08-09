baaa57d9af9624e34279d9526f03f1b1eae51016
unknown <xogk1128@naver.com>
2022-08-09 14:46:34 +0900

    if문 이용

--- MyApp/src/AccountingApp.java
+++ MyApp/src/AccountingApp.java
@@ -11,10 +11,21 @@ public class AccountingApp {
 		double total = valueOfSupply + vat;
 		double expense = valueOfSupply * expenseRate;
 		double income = valueOfSupply - expense;
+
+		double dividend1;
+		double dividend2;
+		double dividend3;
 		
-		double dividend1 = income * 0.5;
-		double dividend2 = income * 0.3;
-		double dividend3 = income * 0.2;
+		if(income > 10000) {
+			dividend1 = income * 0.5;
+			dividend2 = income * 0.3;
+			dividend3 = income * 0.2;
+		} else {
+			dividend1 = income * 1.0;
+			dividend2 = income * 0;
+			dividend3 = income * 0;
+		}
+		
 		
 		System.out.println("Value of supply : " + valueOfSupply);
 		System.out.println("VAT :  : " + vat);
