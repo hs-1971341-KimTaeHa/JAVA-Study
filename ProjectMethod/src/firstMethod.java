d97ac5e193637410f0ce4e3c94eb408bb86bb25f
unknown <xogk1128@naver.com>
2022-08-10 17:25:06 +0900

    method에서 return하기

--- ProjectMethod/src/firstMethod.java
+++ ProjectMethod/src/firstMethod.java
@@ -1,19 +1,20 @@
 import javax.swing.JOptionPane;
 
 public class firstMethod {
-	public static void printTwoTimesA(String text, String delimiter) {
+	public static String twoTimesA(String text, String delimiter) {
 		
-		System.out.println(delimiter);
-		System.out.println(text);
-		System.out.println(text);
+		String out = "";
+		out += delimiter + "\n";
+		out += text + "\n";
+		out += text + "\n";
+		return out;
 	}
 	
 	public static void main(String[] args) {
 		String text = JOptionPane.showInputDialog("");
 		String delimiter = JOptionPane.showInputDialog("");
-		printTwoTimesA(text, delimiter);
-		printTwoTimesA(text, delimiter);
-		printTwoTimesA(text, delimiter);
+		
+		System.out.println(twoTimesA(text, delimiter));
 
 	}
 
