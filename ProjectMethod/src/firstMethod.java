f19740bb09f9e569fea48f6d3ce94613f4165b0c
unknown <xogk1128@naver.com>
2022-08-10 17:12:46 +0900

    parameter 사용

--- ProjectMethod/src/firstMethod.java
+++ ProjectMethod/src/firstMethod.java
@@ -1,15 +1,19 @@
+import javax.swing.JOptionPane;
 
 public class firstMethod {
-	public static void printTwoTimesA() {
-		System.out.println("-");
-		System.out.println("a");
-		System.out.println("a");
+	public static void printTwoTimesA(String text, String delimiter) {
+		
+		System.out.println(delimiter);
+		System.out.println(text);
+		System.out.println(text);
 	}
 	
 	public static void main(String[] args) {
-		printTwoTimesA();
-		printTwoTimesA();
-		printTwoTimesA();
+		String text = JOptionPane.showInputDialog("");
+		String delimiter = JOptionPane.showInputDialog("");
+		printTwoTimesA(text, delimiter);
+		printTwoTimesA(text, delimiter);
+		printTwoTimesA(text, delimiter);
 
 	}
 
