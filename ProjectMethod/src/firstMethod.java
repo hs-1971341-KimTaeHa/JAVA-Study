c100386348f4937b01e35810ad127d49f3edc681
unknown <xogk1128@naver.com>
2022-08-10 17:05:58 +0900

    method 기본 형식

---
+++ ProjectMethod/src/firstMethod.java
@@ -0,0 +1,16 @@
+
+public class firstMethod {
+	public static void printTwoTimesA() {
+		System.out.println("-");
+		System.out.println("a");
+		System.out.println("a");
+	}
+	
+	public static void main(String[] args) {
+		printTwoTimesA();
+		printTwoTimesA();
+		printTwoTimesA();
+
+	}
+
+}
