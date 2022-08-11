3407f057bd5396b905225045b35d43fd840e6f54
unknown <xogk1128@naver.com>
2022-08-11 14:34:02 +0900

    생성자

--- ProjectMethod/src/InheritanceApp.java
+++ ProjectMethod/src/InheritanceApp.java
@@ -1,23 +1,22 @@
 class Cal {
-	public int sum(int v1, int v2) {
-		return v1+v2;
+	int v1, v2;
+	Cal(int v1, int v2){
+		System.out.println("Cal init!!");
+		this.v1 = v1;
+		this.v2 = v2;
 	}
-	
-	// overloading 함주 중복
-	public int sum(int v1, int v2, int v3) {
-		return this.sum(v1, v2)+v3;
+	public int sum() {
+		return this.v1+this.v2;
 	}
 }
 
 class Cal3 extends Cal {
-	public int minus(int v1, int v2) {
-		return v1-v2;
+	Cal3(int v1, int v2){
+		super(v1, v2);
+		System.out.println("Cal3 init!!");
 	}
-	
-	// overriding 함수 재정의
-	public int sum(int v1, int v2) {
-		System.out.println("Cal3!!");
-		return super.sum(v1, v2);
+	public int minus() {
+		return this.v1 - this.v2;
 	}
 }
 
@@ -25,9 +24,10 @@ class Cal3 extends Cal {
 public class InheritanceApp {
 
 	public static void main(String[] args) {
-		Cal3 c3 = new Cal3();
-		System.out.println(c3.sum(3, 3));
-		System.out.println(c3.minus(3, 3));
+		Cal c = new Cal(2, 1);
+		Cal3 c3 = new Cal3(2, 1);
+		System.out.println(c3.sum());
+		System.out.println(c3.minus());
 	}
 
 }
