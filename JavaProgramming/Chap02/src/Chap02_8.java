cc8a46e8e2e63725a76621dab419d7203959c7c8
Taeha Kim <xogk1128@naver.com>
2022-10-16 23:55:37 +0900

    Chap02

--- JavaProgramming/Chap02/src/Chap02_8.java
+++ JavaProgramming/Chap02/src/Chap02_8.java
@@ -1,9 +1,22 @@
-
+import java.util.Scanner;
 public class Chap02_8 {
 
+	public static boolean inRect(int x,int y) {
+		if((x >= 100 && x <= 200) || (y >= 100 && y <= 200))return true;
+		else return false;
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("hi");
-
+		Scanner s = new Scanner(System.in);
+		System.out.print("(x1,y1),(x2,y2) >>");
+		int x1 = s.nextInt();
+		int y1 = s.nextInt();
+		int x2 = s.nextInt();
+		int y2 = s.nextInt();
+		if(inRect(x1,y1) || inRect(x2,y2)) {
+			System.out.println("충돌");
+		}
+		else System.out.println("충돌 x");
 	}
 
 }
