d82bef3f4495c916038d5895988712c24e6af144
Taeha Kim <xogk1128@naver.com>
2022-10-16 23:43:35 +0900

    Chap02

--- JavaProgramming/Chap02/src/Chap02_6.java
+++ JavaProgramming/Chap02/src/Chap02_6.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class problem {
+public class Chap02_6 {
 	public static void main(String args[]) {
 		Scanner s = new Scanner(System.in);
 		System.out.print("1~99 사이의 정수를 입력하시오 >>");
---
+++ JavaProgramming/Chap02/src/Chap02_8.java
@@ -0,0 +1,9 @@
+
+public class Chap02_8 {
+
+	public static void main(String[] args) {
+		System.out.println("hi");
+
+	}
+
+}
