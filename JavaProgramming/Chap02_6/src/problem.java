28dde1d3d3ae31cb0c1eae2e4e4954b207c43fa0
Taeha Kim <xogk1128@naver.com>
2022-10-16 23:37:24 +0900

    02-6

---
+++ JavaProgramming/Chap02_6/src/problem.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class problem {
+	public static void main(String args[]) {
+		Scanner s = new Scanner(System.in);
+		System.out.print("1~99 사이의 정수를 입력하시오 >>");
+		int num = s.nextInt();
+
+		if(((num%10 != 0) && ((num%10) % 3 == 0)) && ((num/10) % 3 == 0)) {
+			System.out.println("박수짝짝");
+		}
+		else if(((num%10) % 3 == 0) || ((num/10) % 3 == 0)) {
+			System.out.println("박수짝");
+		}
+	}
+}
